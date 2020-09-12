fd456b84eac48e41ccac8c3192f0e214539e365d
@MrMoraschi <68395930+MrMoraschi@users.noreply.github.com>
2020-09-12 19:57:16 +0200

    Add files via upload

---
+++ AFK.js
@@ -0,0 +1,222 @@
+var mineflayer = require('mineflayer')
+var notifier = require('node-notifier');
+var bloodhoundPlugin = require('mineflayer-bloodhound')(mineflayer);
+var { pathfinder, Movements } = require('mineflayer-pathfinder');
+const mineflayerDashboard = require('mineflayer-dashboard');
+var { GoalNear, GoalBlock, GoalXZ, GoalY, GoalInvert, GoalFollow } = require('mineflayer-pathfinder').goals
+var incomingnotification
+
+if (process.argv.length < 6 || process.argv.length > 8) {
+  console.log('Usage : AFK.js \x1b[31m<host>\x1b[0m')
+  console.log('               \x1b[31m<port>\x1b[0m')
+  console.log('               \x1b[31m<choose from yes/no if you want or not Windows notifications for commands like /tell or events>\x1b[0m')
+  console.log('               \x1b[31m<username of the owner of the bot, so the bot replies only to him>\x1b[0m')
+  console.log('               \x1b[32m[<name, gmail in case the account is premium>]\x1b[0m')
+  console.log('               \x1b[32m[<password, ignore in case the account is cracked>]\x1b[0m')
+  console.log('')
+  console.log('               \x1b[31mRed\x1b[0m: Needed, \x1b[32mGreen\x1b[0m: Optional')
+  console.log('')
+  console.log('               \x1b[36mExample: node AFK.js localhost 25565\x1b[0m \x1b[4m(this makes a cracked account bot that connects to localhost)\x1b[0m')
+  process.exit(1)
+}
+
+startBot()
+
+function startBot() {
+  var bot = mineflayer.createBot({
+    host: process.argv[2],
+    port: parseInt(process.argv[3]),
+    username: process.argv[6] ? process.argv[6] : 'AFKBot',
+    password: process.argv[7]
+  })
+
+  //LOAD PLUGIN, FUNCTIONS
+  bot.loadPlugin(pathfinder)
+  
+  var incomingnotification = process.argv[4]
+  var owner = process.argv[5]
+
+  bot.on('login', function() {
+    bot.loadPlugin(require('mineflayer-dashboard'))
+  })
+
+
+
+  //LOOKING
+  bot.once('login', function () {
+    setInterval(() => {
+        const entity = bot.nearestEntity()
+          if (entity !== null) {
+            if (entity.type === 'player') {
+            bot.lookAt(entity.position.offset(0, 1.6, 0))
+            } else if (entity.type === 'mob') {
+              bot.lookAt(entity.position)
+            }
+          }
+        }, 50)
+    bot.on('kicked', () => {
+      return
+    })
+  })
+
+
+
+  //CHAT
+  bot.on('whisper', (username, message, rawMessage) => {
+      if (message === 'follow me') {
+        if (username === owner) {
+          bot.whisper(username, 'On my way')
+      }} else if (message === 'stop') {
+          null
+      } else {
+          if (message !== 'follow me' && message !== 'stop')
+            bot.whisper(username, 'Sorry, I am an AFK Bot')
+            bot.dashboard.log('\x1b[32m<STATUS> Correctly whispered that I am a bot','\x1b[0m')
+      }
+          if (incomingnotification === 'yes') {
+            if (username !== bot.username) {
+              if (username !== 'You') {
+                if (message !== 'follow me' && message !== 'stop') {
+                  if (message !== 'Sorry, I am an AFK Bot') {
+                    notifier.notify({
+                      title: 'Whisper Message',
+                      message: ('You have a new message'),
+                      icon: 'logo.png'
+                    })
+                  }
+                }
+              }
+            }
+          }    
+    
+  })
+
+
+
+  //KICK & ERRORS
+  bot.on('kicked', (reason, loggedIn) => {
+    bot.dashboard.log('\x1b[32m<STATUS>\x1b[0m \x1b[31mI got kicked for','\x1b[0m',reason, loggedIn)
+    if (incomingnotification === 'yes') {
+      notifier.notify({
+        title: 'Event Message',
+        message: ('I got kicked!'),
+        icon: 'logo.png'
+      })
+      }
+  })
+  bot.on('kicked', () =>
+    setTimeout(() => {
+      startBot()
+    }, 10000)
+  )
+
+
+  
+  //EVENTS
+  bot.on('death', () => {
+    bot.dashboard.log(`\x1b[32m<STATUS>\x1b[0m \x1b[31mI died`,'\x1b[0m')
+    if (incomingnotification === 'yes') {
+          notifier.notify({
+            title: 'Event Message',
+            message: ('I died!'),
+            icon: 'logo.png'
+          })
+          }
+        }
+  )
+
+  bot.once('spawn', () => {
+    bot.dashboard.log(`\x1b[32m<STATUS> Correctly spawned at ${bot.entity.position}`,'\x1b[0m')
+  })
+
+  bot.on('respawn', () => {
+    bot.dashboard.log(`\x1b[32m<STATUS> Correctly respawned at ${bot.entity.position}`,'\x1b[0m')
+  })
+
+  bot.on('login', () => {
+    bot.dashboard.log('\x1b[32m<STATUS> Correctly logged in','\x1b[0m')
+  })
+
+  bot.once('health', () => {
+    bot.dashboard.log(`\x1b[32m<STATUS> I have ${bot.health} health.`,'\x1b[0m')
+  })
+
+  bot.on('health', () => {
+    if (bot.health <= 5)
+      bot.dashboard.log(`\x1b[32m<STATUS> \x1b[33mMy remaining health is ${bot.health}`,'\x1b[0m')
+  })
+
+
+
+  //WORLD
+  bot.once('time', () => {
+    setTimeout(function() {
+    bot.dashboard.log(`\x1b[36m<WORLD> \x1b[36mCurrent time: `+bot.time.timeOfDay,'\x1b[0m')
+  }, 1000)})
+
+  bot.on('playerJoined', (player) => {
+    setTimeout(function() {
+    if (player.username !== bot.username) {
+      bot.dashboard.log(`\x1b[36m<WORLD>\x1b[0m \x1b[32m${player.username} joined`,`\x1b[0m`)
+    }
+  }, 3000)})
+
+  bot.on('playerLeft', (player) => {
+    if (player.username === bot.username) return
+    bot.dashboard.log(`\x1b[36m<WORLD>\x1b[0m \x1b[32m${player.username} left`,`\x1b[0m`)
+  })
+
+
+
+  //PATHFIND
+  bot.once('spawn', () => {
+    
+    const mcData = require('minecraft-data')(bot.version)
+
+    const defaultMove = new Movements(bot, mcData)
+    defaultMove.allowFreeMotion = true
+
+    bot.on('whisper', (username, message) => {
+      if (username === bot.username) return
+
+      const target = bot.players[username].entity
+      if (username === owner) {
+        if (message === 'follow me') {
+          bot.pathfinder.setMovements(defaultMove)
+          bot.pathfinder.setGoal(new GoalFollow(target, 2), true)
+        } else if (message === 'stop') {
+          bot.pathfinder.setGoal(null)
+        }
+      }
+    })
+  })
+
+
+
+  //AFK
+  bot.on('spawn', () => {
+    bot.dashboard.log('\x1b[32m<STATUS> Starting anti-AFK-kick sequence','\x1b[0m')
+    setInterval(() => {
+      setTimeout(() => {
+       bot.setControlState('jump', false)
+      }, 100);
+      bot.setControlState('jump', true)
+    }, 600000);
+  })
+
+
+
+  //BLOODHOUND
+  bloodhoundPlugin(bot);
+
+  bot.bloodhound.yaw_correlation_enabled = true;
+
+  bot.on('onCorrelateAttack', function (attacker,victim,weapon) {
+    if (bot.username === victim.username) {
+      if (weapon) {
+          bot.dashboard.log('\x1b[32m<STATUS>\x1b[0m \x1b[33mI got hurt by ' + (attacker.displayName|| attacker.username) + ' with a/an ' + (weapon.displayName),'\x1b[0m');
+        } else {
+          bot.dashboard.log('\x1b[32m<STATUS>\x1b[0m \x1b[33mI got hurt by ' + (attacker.displayName|| attacker.username),'\x1b[0m');
+        }
+  }});
+}
